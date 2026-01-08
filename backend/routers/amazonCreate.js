980d0d743272fd4b3ff617aeb91d106348aa9472
gE <>
2026-01-08 13:15:30 +0800

    忘了昨天做什么了

--- backend/routers/amazonCreate.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const AmazonCreateController = require("../controllers/amazonCreate.js");
-
-router.post('/:fileid', AmazonCreateController.createAmazonBoard);
-
-module.exports = router;
--- backend/routers/amazonSearch.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const AmazonSearchController = require("../controllers/amazonSearch.js");
-
-router.get('/list', AmazonSearchController.listAmazonBoards);
-router.get('/:fileid', AmazonSearchController.getAmazonBoard);
-
-module.exports = router;
